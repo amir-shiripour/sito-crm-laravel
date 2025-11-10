9e9b47491c9cae0960eb6401bf1da7276426adc4
Amir Shiripour <161520333+amir-shiripour@users.noreply.github.com>
2025-11-10 18:04:39 +0330

    fix Tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,28 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import defaultTheme from 'tailwindcss/defaultTheme'
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
+import tailwindcssRtl from 'tailwindcss-rtl'
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-    ],
+        './resources/js/**/*.js',
 
+        // ⬇️ اضافه کن (ساختار nwidart modules)
+        './Modules/**/Resources/views/**/*.blade.php',
+        './Modules/**/Resources/js/**/*.js',
+        './Modules/**/Resources/**/*.vue', // اگر Vue داری
+        './Modules/**/Resources/**/*.tsx', // اگر React/TSX داری
+    ],
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Vazirmatn', ...defaultTheme.fontFamily.sans],
-            },
+            fontFamily: { sans: ['Vazirmatn', ...defaultTheme.fontFamily.sans] },
         },
     },
-
-    plugins: [forms, typography],
-};
-
-module.exports = {
-    darkMode: 'class',
-    content: [
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.js',
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [
-        require('tailwindcss-rtl'), // npm i -D tailwindcss-rtl
-    ],
+    plugins: [forms, typography, tailwindcssRtl],
 }
-
