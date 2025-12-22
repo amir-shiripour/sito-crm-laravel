a2783dadfc2c2341e10f30544e5d7ba5ad7b01d3
Amir Shiripour <161520333+amir-shiripour@users.noreply.github.com>
2025-12-22 16:16:24 +0330

    Restore wizard step getters for appointment create

--- modules/Booking/Resources/menu.php
+++ modules/Booking/Resources/menu.php
@@ -51,6 +51,21 @@ return [
     ],
 
     [
+        'title'      => 'فرم‌ها',
+        'route'      => 'user.booking.forms.index',
+        'permission' => 'booking.forms.view',
+        'icon'       => '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <rect x="4" y="4" width="16" height="16" rx="2" />
+            <path d="M8 8h8" />
+            <path d="M8 12h8" />
+            <path d="M8 16h4" />
+        </svg>',
+        'group'     => 'booking',
+        'position'  => 12.6,
+    ],
+
+    [
         'title' => 'برنامه زمانی ارائه‌دهندگان',
         'route' => 'user.booking.providers.index',
         'permission' => 'booking.availability.manage',
